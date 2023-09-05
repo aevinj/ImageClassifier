dec68e99b2a0af13d22e0c53cdcc079d6af3103c
aevinj <ajjaevinjais@gmail.com>
2023-09-05 20:59:29 +0100

    load dataset & clean dataset convert jpg to jpeg

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import tensorflow as tf
-import matplotlib.pyplot as plt
-import cv2, os
+from matplotlib import pyplot as plt
+import cv2, os, subprocess
+import numpy as np
 
 class ImageClassifier:
     def __init__(self) -> None:
         self.DATA_DIRECTORY = 'data'
-        self.IMG_EXTENSIONS = ['jpg', 'jpeg', 'bmp', 'png']
+        # Supported image formats: jpeg, png, bmp - for keras
+        # png causing libpng errors so disregarded as well
+        self.IMG_EXTENSIONS = ['jpeg', 'bmp']
+
+    def get_DATA_DIRECTORY(self):
+        return self.DATA_DIRECTORY
+    
+    def get_IMG_EXTENSIONS(self):
+        return self.IMG_EXTENSIONS
 
     def get_file_extension(self, file_path):
         # Split the file path into its base name and extension
@@ -13,17 +22,32 @@ class ImageClassifier:
         
         # Remove the leading dot (.) from the extension
         extension = extension.lstrip('.')
-        
         return extension
 
-    def cleanData(self):
-        for image_class in os.listdir(self.DATA_DIRECTORY): 
-            for image in os.listdir(os.path.join(self.DATA_DIRECTORY, image_class)):
-                image_path = os.path.join(self.DATA_DIRECTORY, image_class, image)
-                try: 
-                    img = cv2.imread(image_path)
+    def cleanDataset(self):
+        for image_class in os.listdir(self.get_DATA_DIRECTORY()): 
+            for image in os.listdir(os.path.join(self.get_DATA_DIRECTORY(), image_class)):
+                image_path = os.path.join(self.get_DATA_DIRECTORY(), image_class, image)
+                try:
+                    # To check if the file can be opened and has the right extension :)                    
+                    if image.endswith('.jpg'):
+                        img = cv2.imread(image_path)
+                        cv2.imwrite(os.path.join(self.get_DATA_DIRECTORY(), image_class, image[:-3] + ".jpeg"), img)
+                        
                     ext = self.get_file_extension(image_path)
-                    if ext not in self.IMG_EXTENSIONS:
+                    if ext not in self.get_IMG_EXTENSIONS():
                         os.remove(image_path)
-                except Exception as e: 
+                except Exception as e:
                     os.remove(image_path)
+                    
+    def loadDataset(self):
+        data = tf.keras.utils.image_dataset_from_directory(self.get_DATA_DIRECTORY())
+        data_iterator = data.as_numpy_iterator()
+        batch = data_iterator.next()
+        print(batch[1])
+        
+                    
+if __name__ == '__main__':
+    ic = ImageClassifier()
+    ic.cleanDataset()
+    ic.loadDataset()
