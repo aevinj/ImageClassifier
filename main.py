ca7406da3c7a4570ffa4f0d7604a51a941ac2e61
aevinj <ajjaevinjais@gmail.com>
2023-09-05 19:24:18 +0100

    conversion to class structure

--- main.py
+++ main.py
@@ -2,25 +2,28 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import cv2, os
 
-DATA_DIRECTORY = 'data'
-IMG_EXTENSIONS = ['jpg', 'jpeg', 'bmp', 'png']
+class ImageClassifier:
+    def __init__(self) -> None:
+        self.DATA_DIRECTORY = 'data'
+        self.IMG_EXTENSIONS = ['jpg', 'jpeg', 'bmp', 'png']
 
-def get_file_extension(file_path):
-    # Split the file path into its base name and extension
-    base_name, extension = os.path.splitext(file_path)
-    
-    # Remove the leading dot (.) from the extension
-    extension = extension.lstrip('.')
-    
-    return extension
+    def get_file_extension(self, file_path):
+        # Split the file path into its base name and extension
+        base_name, extension = os.path.splitext(file_path)
+        
+        # Remove the leading dot (.) from the extension
+        extension = extension.lstrip('.')
+        
+        return extension
 
-for image_class in os.listdir(DATA_DIRECTORY): 
-    for image in os.listdir(os.path.join(DATA_DIRECTORY, image_class)):
-        image_path = os.path.join(DATA_DIRECTORY, image_class, image)
-        try: 
-            img = cv2.imread(image_path)
-            ext = get_file_extension(image_path)
-            if ext not in IMG_EXTENSIONS:
-                os.remove(image_path)
-        except Exception as e: 
-            os.remove(image_path)
+    def cleanData(self):
+        for image_class in os.listdir(self.DATA_DIRECTORY): 
+            for image in os.listdir(os.path.join(self.DATA_DIRECTORY, image_class)):
+                image_path = os.path.join(self.DATA_DIRECTORY, image_class, image)
+                try: 
+                    img = cv2.imread(image_path)
+                    ext = self.get_file_extension(image_path)
+                    if ext not in self.IMG_EXTENSIONS:
+                        os.remove(image_path)
+                except Exception as e: 
+                    os.remove(image_path)
